196ee265a8f095a3ad1cb135147396986f26e4fe
a-moseman <drewmoseman@gmail.com>
2023-11-29 16:00:22 -0500

    Initial commit

---
+++ src/main/java/org/amoseman/cmdb/Configuration.java
@@ -0,0 +1,2 @@
+package org.amoseman.cmdb;public class Configuration {
+}
---
+++ src/main/java/org/amoseman/cmdb/Main.java
@@ -0,0 +1,7 @@
+package org.amoseman;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
